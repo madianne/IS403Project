8d4f3e72286abab0b8a1fe8070e990e494340e7a
madianne <madi.hamilton1@gmail.com>
2017-11-07 17:18:12 -0700

    commit

---
+++ FinancialReportingPage(Project1)/App_Start/FilterConfig.cs
@@ -0,0 +1,13 @@
+﻿using System.Web;
+using System.Web.Mvc;
+
+namespace FinancialReportingPage_Project1_
+{
+    public class FilterConfig
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+    }
+}
---
+++ FinancialReportingPage(Project1)/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="FinancialReportingPage_Project1_.MvcApplication" Language="C#" %>
